545d12047a8df149951306fd9fddf5c25eec6c1a
nathanferrell <46502922+nathanferrell@users.noreply.github.com>
2024-10-21 16:17:03 -0500

    Update decode.v

--- decode.v
+++ decode.v
@@ -3,36 +3,37 @@
 module decode(inst_encoding, next_pc_sel);
  
 input wire [31:0] inst_encoding;
-output reg[1:0] next_pc_sel;
+output reg[2:0] next_pc_sel;
  
 always @(inst_encoding) begin
 casex(inst_encoding)
  
+//are next_pc_sel set equal to the correct tic defines?
 `INST_ENCODING_JAL: begin
-next_pc_sel = `NEXT_PC_FROM_JAL_IMM;
+next_pc_sel = `PC_PLUS_JAL_IMM;
  
 end
  
  
 `INST_ENCODING_JALR:begin
  
-next_pc_sel = `NEXT_PC_FROM_JALR;
+next_pc_sel = `NEXT_PC_FROM_RF;
  
 end
  
  
 `INST_ENCODING_BEQ:begin
-next_pc_sel = `NEXT_PC_FROM_BRANCH;
+next_pc_sel = `PC_PLUS_BRCH_IMM;
  
 end
  
 `INST_ENCODING_ADDI:begin
-next_pc_sel = `NEXT_PC_PLUS_4;
+next_pc_sel = `PC_FROM_PC_PLUS_4;
  
 end
  
 default:begin
-next_pc_sel = `NEXT_PC_PLUS_4;
+next_pc_sel = `PC_FROM_PC_PLUS_4;
  
 end
  
