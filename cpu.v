efdc05dd21973ebb49071a0a138eab2903a655fa
nathanferrell <46502922+nathanferrell@users.noreply.github.com>
2024-10-23 11:21:37 -0500

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -14,6 +14,8 @@ wire[31:0] rf_out_0;
  
 wire[31:0] rf_out_1;
  
+wire[31:0] Alu_in_2;
+ 
  
 wire[31:0] immediatejal;
  
@@ -23,11 +25,10 @@ wire[31:0] immediatelui;
  
 wire[31:0] immediatebrch;
  
+wire[2:0] S;
  
 wire[2:0] pc_sel;
-
 wire pc_write_enable;
-
 reg[31:0] next_pc;
  
 wire[31:0]pc_plus_4 = pc + 32'd4;
@@ -37,9 +38,7 @@ wire[31:0] pc_from_rf= rf_out_0;
  
  
 always @(*)begin
-
 case(pc_sel)
-
 `PC_FROM_PC_PLUS_4: next_pc = pc_plus_4;
  
 `PC_PLUS_JAL_IMM: next_pc = pc + immediatejal;
@@ -68,19 +67,19 @@ memory2c mem(.data_out(inst_encoding), .data_in(32'b0), .addr(pc), .enable(1'b1)
  
 RegFile rgf(.ra0(inst_encoding[19:15]), .ra1(inst_encoding[24:20]), .wa(inst_encoding[11:7]), .wd(32'b0), .we(32'b0), .clk(clk), .rst(rst), .do0(rf_out_0), .do1(rf_out_1));
  
+ALU al(.input_one(rf_out_0), .input_two(Alu_in_2), .func(), .out(), .zeroflg(), .cout());
+ 
+assign Alu_in_2 = S ? rf_out_1:immediatebrch;
+ 
  
 decode d(.inst_encoding(inst_encoding), .next_pc_sel(pc_sel));
  
 //Mux8to1 mx(.inp0(), .inp1(), .inp2(), .inp3(), .inp4(), .inp5(), .inp6(), .inp7(), .sel(pc_sel),.out(next_pc));
  
 assign immediatejal = {{13{inst_encoding[31]}}, inst_encoding[19:12], inst_encoding[20], inst_encoding[30:21], 1'b0};
-
 assign immediateaiupc = {inst_encoding[31:12], 12'b0};
-
 assign immediatelui = {inst_encoding[31:12],12'b0};
-
 assign immediatebrch = {{20{inst_encoding[12]}}, inst_encoding[7], inst_encoding[30:25], inst_encoding[11:8], 1'b0};
 
-//do we need an immediate for register?                              
  
 endmodule
